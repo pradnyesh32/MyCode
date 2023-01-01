e9921cca5a791be0c37d51af87ccb58f2d4e7fe5
Pradnyesh Kadam <112795162+pradnyesh32@users.noreply.github.com>
2023-01-01 12:22:16 +0530

    Delete hapynewyer directory

--- hapynewyer/app.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var app = express();
-
-app.get("/",function(req,resp){
-    resp.send("Hii...");
-});
-
-app.listen(6000);
-console.log("server running at port 6000");
