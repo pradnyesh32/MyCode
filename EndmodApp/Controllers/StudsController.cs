26d78b1b6e8c73b1e6d584ac0938b5029be0b8ee
Pradnyesh Kadam <112795162+pradnyesh32@users.noreply.github.com>
2023-01-17 01:19:18 +0530

    Add files via upload

---
+++ EndmodApp/Controllers/StudsController.cs
@@ -0,0 +1,50 @@
+
+using Microsoft.AspNetCore.Mvc;
+using EndmodApp.Models;
+using stud;
+using db;
+namespace EndmodApp.Controllers;
+
+public class StudsController : Controller
+{
+    private readonly ILogger<StudsController> _logger;
+
+    public StudsController(ILogger<StudsController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+     public IActionResult Studlist()
+    {
+        List<Student> studs = DB.GetAllStudents();
+        foreach(Student s in studs){
+            Console.WriteLine(s.Id);
+        }
+        ViewData["prop"]=studs;
+        return View();
+    }
+
+     public IActionResult Addstud()
+    {
+        return View();
+    }
+
+     public IActionResult Insert()
+    {   
+        var id = Convert.ToInt32(Request.Query["id"]);
+        var name=Request.Query["name"];
+        var c=Request.Query["course"];
+        Student s = new Student(id,name,c);
+        DB.SaveNewUser(s);
+    
+        return Redirect("Studlist");
+    }
+
+
+
+}
